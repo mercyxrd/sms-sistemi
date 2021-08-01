05665a09970d14dcc4e2bf493b64aa14ba210508
Mêrcy <78019226+mercyxrd@users.noreply.github.com>
2021-08-01 14:45:33 +0300

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,11 +1,21 @@
 module.exports = {
     mercy: {
-        guildID:     "",
-        api_key:     "", // projenin readme dosyasını okuyun xd
-        botToken:    "",
-        api_secret:  "", // projenin readme dosyasını okuyun xd
-        phoneNumber: "",
+        guildID:     "842833675795169340",
+        api_key:     "xx", // projenin readme dosyasını okuyun xd
+        botToken:    "xx",
+        botOwner:    "411621794131476480",
+        api_secret:  "xx", // projenin readme dosyasını okuyun xd
+        phoneNumber: ":)",
         virtlNumber: "",
 
+    },
+
+    logs: {
+        guard_log:   "862777340043984936",
+    },
+
+    roles: {
+        jail:    "", 
+        booster: "",
     }
 }
